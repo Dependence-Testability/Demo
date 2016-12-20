63fd088674c8d88eca4dc3aac22e5ca27c9aa93a
dmariselli <dmariselli@gmail.com>
2016-12-20 15:21:45 -0500

    updated APLTester

--- APLTester.java
+++ APLTester.java
@@ -8,7 +8,7 @@ public class APLTester {
 
   public static void main(String[] args) {
     Graph<Integer> graph = new Graph<>();
-    // int[][] edges
+    int[][] edges
         // = {{0, 1}, {1, 2}, {2, 3}, {3, 2},
         //    {3, 7}, {7, 3}, {2, 6}, {7, 6},
         //    {5, 6}, {6, 5}, {1, 5}, {4, 5},
@@ -34,9 +34,9 @@ public class APLTester {
         // 16 paths from 1 to 5
         // Avg path length is 3
 
-        // = {{1, 2}, {1, 3}, {1, 4}, {2, 3},
-        //    {2, 4}, {2, 5}, {3, 2}, {3, 4}, 
-        //    {3, 5}, {4, 2}, {4, 3}, {4, 5}};
+        = {{1, 2}, {1, 3}, {1, 4}, {2, 3},
+           {2, 4}, {2, 5}, {3, 2}, {3, 4}, 
+           {3, 5}, {4, 2}, {4, 3}, {4, 5}};
         // 3 Strongly Connected Components
         // 15 paths from 1 to 5
         // Avg path length is 3.2
@@ -50,12 +50,15 @@ public class APLTester {
 
     
 
-    // for (int[] edge : edges) {
-    //   graph.addNewEdge(edge[0], edge[1]);
-    // }
+    for (int[] edge : edges) {
+      graph.addNewEdge(edge[0], edge[1]);
+    }
 
     // analyze("random_dag.txt", 1, 5);
-    analyze("random_graph.txt", 11, 12);
+    // analyze("random_graph.txt", 11, 12);
+    double[] result = APL.compute(graph, 1, 5, 2, 10);
+    System.out.println("Number of paths is " + result[0]);
+    System.out.println("Average length of paths is " + result[1]);
 
     // TarjanSCC<Integer> scc = new TarjanSCC<Integer>(graph);
     // System.out.println(scc.count() + " Strongly Connected Components");
