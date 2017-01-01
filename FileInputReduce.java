d939b0d67e452f32a70939c3044d2d37f41824db
ooniorisan17 <ooniorisan17@amherst.edu>
2016-12-31 22:48:19 -0500

    Code now compiles for me on Hadoop2

--- FileInputReduce.java
+++ FileInputReduce.java
@@ -26,7 +26,7 @@ import estimator.general.*;
  public static int init_source;
     public static int init_dest;
     public static Graph<Integer> subGraph;
-    //public static Context public_context;
+    public static Context public_context;
     public static String public_prefSuf;
     public static String toExclude_public;
 
